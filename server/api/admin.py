c50853b922fc233b1882456e36bd408b89eb42ef
Matthew Antoun <matthew@antoun.org>
2016-01-30 10:24:43 -0800

    Add Django admin for Events

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Comment, Event
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'datetime', 'location')
+    list_filter = ('name', 'owner', 'datetime', 'location')
+    inlines = [CommentInline, ]
+
+
+admin.site.register(Event, EventAdmin)
