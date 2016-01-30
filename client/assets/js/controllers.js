21e059ab8d6f473bcf59943c2922d816bb56e9ae
ALSpringer <aaronlspringer@gmail.com>
2016-01-30 15:56:11 -0800

    Temp commit of main

--- client/assets/js/controllers.js
+++ client/assets/js/controllers.js
@@ -43,6 +43,21 @@
       $scope.makeQuery();
     };
   }
+    
+    mcraControllers.controller('MainController', function($scope) {
+      $scope.data = {
+        selectedIndex: 0,
+        secondLocked:  true,
+        secondLabel:   "Item Two",
+        bottom:        false
+      };
+      $scope.next = function() {
+        $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2) ;
+      };
+      $scope.previous = function() {
+        $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
+      };
+    });
 
     mcraControllers.controller('EventDetailsController', function($scope) {
       var users = [
