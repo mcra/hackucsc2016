3f68006ecdcf613046549dcddc6b27656d976e98
Christopher Antoun <christopher@antoun.org>
2016-01-30 13:22:11 -0800

    Move test event data into json

--- client/assets/js/controllers.js
+++ client/assets/js/controllers.js
@@ -4,33 +4,17 @@
   /* Controllers */
   var mcraControllers = angular.module('mcraControllers', []);
 
-  mcraControllers.controller('EventController', ['$scope', '$filter', EventController]);
+  mcraControllers.controller('EventController', ['$scope', '$http', '$filter', EventController]);
 
-  function EventController($scope, $filter) {
+  function EventController($scope, $http, $filter) {
     $scope.title = "Get Bizzy";
     $scope.query = { name: '', location: '', date: '', };
 
     // TODO some test data; get from the API
-    $scope.events = [
-      { name: 'biking',
-        location: 'here',
-        date: 'today',
-      },
-      { name: 'surfing',
-        location: 'there',
-        date: 'tomorrow',
-      },
-      { name: 'dranks',
-        location: 'the old bar',
-        date: 'tomorrow',
-      },
-      { name: 'tai chi',
-        location: 'park',
-        date: 'tomorrow',
-      },
-    ];
-
-    $scope.filtered = $scope.events;
+    $http.get('assets/js/test-events.json').success(function(data) {
+      $scope.events = data;
+      $scope.filtered = $scope.events;
+    });
 
     $scope.makeQuery = function() {
       $scope.filtered = $filter('filter')($scope.events, $scope.query);
