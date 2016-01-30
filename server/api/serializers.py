d305fe23f6e4e575768f0c3182d57059d4cc15f3
Matthew Antoun <matthew@antoun.org>
2016-01-29 23:55:33 -0800

    Add Comment detail routes to the Event endpoint

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
 
-from api.models import Event
+from api.models import Event, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    A Comment serializer.
+    """
+    # TODO: serialize with owner name
+
+    class Meta:
+        model = Comment
+        exclude = ('id',)
+        read_only = ('event',)
 
 
 class EventSerializer(serializers.ModelSerializer):
@@ -9,6 +21,7 @@ class EventSerializer(serializers.ModelSerializer):
     """
     # TODO: serialize members by username
     # TODO: don't show all possible members in the browseable API creation form
+    # TODO: exclude members. they can't be designated at creation
 
     class Meta:
         model = Event
--- server/api/views.py
+++ server/api/views.py
@@ -1,7 +1,10 @@
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, status, viewsets
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 
-from api.models import Event
-from api.serializers import EventSerializer
+from api.models import Event, Comment
+from api.permissions import IsOwner
+from api.serializers import CommentSerializer, EventSerializer
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -14,6 +17,39 @@ class EventViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Event.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    @detail_route(methods=['GET', 'POST'])
+    def comments(self, request, pk=None):
+        if request.method == 'GET':
+            comments = CommentSerializer(
+                Comment.objects.filter(event=pk), many=True)
+            resp = {'comments': comments.data}
+            return Response(resp)
+        elif request.method == 'POST':
+            serializer = CommentSerializer(data=request.data)
+            serializer.initial_data['event'] = pk
+            if serializer.is_valid():
+                serializer.save(owner=self.request.user)
+                return Response({'status': 'OK'})
+            else:
+                return Response(serializer.errors,
+                                status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions. List only owned comments.
+    """
+    # TODO: only list -- don't allow creation from here
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+    def get_queryset(self):
+        return Comment.objects.filter(owner=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
