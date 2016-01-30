ebb98988b743a6d6801b4502db261f1bd33cfa28
Matthew Antoun <matthew@antoun.org>
2016-01-29 22:46:54 -0800

    Create API tokens on User creation

--- server/api/models.py
+++ server/api/models.py
@@ -1,7 +1,11 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.timezone import now
+from rest_framework.authtoken.models import Token
 
 
 class Event(models.Model):
@@ -39,3 +43,10 @@ class Prefs(models.Model):
     # A history of joined events?
     # A picture
     owner = models.OneToOneField('auth.User', related_name='prefs')
+
+
+# Generate API tokens when users are created
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
