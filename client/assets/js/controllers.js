6ec5d7d06c56f83aaf6e4459b441db5288569e33
Matthew Antoun <matthew@antoun.org>
2016-01-31 09:02:51 -0800

    Implement joining and leaving events

--- client/assets/js/controllers.js
+++ client/assets/js/controllers.js
@@ -39,17 +39,27 @@
     };
   }
     
-  mcraControllers.controller('EventDetailsController', function($scope, $http, $routeParams, $location, api, Event, Members, Comments) {
+  mcraControllers.controller('EventDetailsController', function($scope, $http, $routeParams, $cookieStore, $location, api, Event, Members, Comments) {
     if (!api.init()) { $location.path('/login'); } // force log in
 
     var evtId = $routeParams.eventId;
     $scope.event = Event.query({id: evtId});
-    $scope.users = Members.query({id: evtId});
+    $scope.users = Members.query({id: evtId}, function(res) {
+      $scope.joined = false;
+      var cur = $cookieStore.get('username');
+      for (var i=0; i<res.length; i++) {
+        if (cur === res[i].username) {
+          $scope.joined = true;
+        }
+      }
+    });
+
     $scope.commentChain = Comments.query({id: evtId});
     $scope.postComment = function() {
       Comments.save({id: evtId, text: $scope.message})
         .$promise.then(function(e) {
           $scope.message = undefined;
+          // TODO: api to return new comment list on post
           $scope.commentChain = Comments.query({id: evtId});
         });
     };
@@ -57,6 +67,18 @@
     $scope.goHome = function() { $location.path('/'); };
     $scope.userDetail = function(id) {
       $location.path('/users/'+id);
+    };
+    $scope.join = function() {
+      Event.join({id: evtId})
+        .$promise.then(function(e) {
+          // TODO: api to return new members list on join
+          $scope.users = Members.query({id: evtId});
+          $scope.joined = true;
+        });
+    };
+    $scope.leave = function() {
+      Event.leave({id: evtId});
+      $location.path('/');
     };
   });
 
@@ -84,6 +106,7 @@
         username: this.username,
         password: this.password
       };
+      $cookieStore.put('username', credentials.username);
       var success = function(data) {
         var token = data.token;
         api.init(token);
@@ -98,6 +121,7 @@
     };
     $scope.logout = function() {
       $cookieStore.remove('token');
+      $cookieStore.remove('username');
       api.logout();
     };
 });
