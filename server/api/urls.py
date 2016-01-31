9257a59fa03abef66a587a15459f0a66dabd09d5
Matthew Antoun <matthew@antoun.org>
2016-01-30 17:00:28 -0800

    Add token auth endpoint

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from rest_framework.authtoken import views as auth_views
 from rest_framework.routers import DefaultRouter
 
 from . import views
@@ -10,4 +11,5 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/',
         include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', auth_views.obtain_auth_token),
 ]
